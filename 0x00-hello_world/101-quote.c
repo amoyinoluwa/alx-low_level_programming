fd0c4f39bb01490e3d472f19a8c14b5c03d98547
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-02 15:26:12 -0700

    --

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	write(1, "and that piece of art is useful\"", 32);
-	write(2, " - Dora Korpar, 2015-10-19", 27);
+	write(2, " - Dora Korpar, 2015-10-19\n", 27);
 	freopen("/dev/null", "w", stderr);
 	return (1);
 }
