4c9163edb06dbe382d2d1196634228046fb6a86b
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-02 15:11:03 -0700

    --

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - program contents
+ *
+ * Return: return value
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\"", 32);
+	write(2, " - Dora Korpar, 2015-10-19,", 27);
+	return (1);
+}
