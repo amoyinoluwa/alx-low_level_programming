1690bc6c8fd4a750769e7a87c6ced42911066a9d
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-02 13:04:59 -0700

    --

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-/* main - This is the entry point of the program
+/**
+ *main - This is the entry point of the program
  * This program outputs the content of a string
  * Return: Always success
  */
