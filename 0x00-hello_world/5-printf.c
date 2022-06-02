5a2c2793e8a43e9edb398fc8705edae1cf4bf90a
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-02 13:12:38 -0700

    --

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * This program demonstrates the printf function
+ */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
