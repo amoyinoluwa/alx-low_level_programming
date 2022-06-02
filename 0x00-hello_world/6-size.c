9df23bd2767ab2c6cfa5c9914cb9e9d06bab1a9f
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-02 14:25:50 -0700

    --

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
  * main - contains main program
- * sizeof - size of data types
- * Return: returns zero
+ *
+ * Return - zero
  */
 
-int main(void)                                                                                                                                 
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
 	return (0);
 }
