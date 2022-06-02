140f46dfe7ff9e5e7e9acd309aefc98012969f5b
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-02 12:58:45 -0700

    --

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-int main(){
-	char str[] ="\"Programming is like building a multilingual puzzle";
+/*
+ * This program outputs the content of a string
+ */
+
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
 	return (0);
 }
