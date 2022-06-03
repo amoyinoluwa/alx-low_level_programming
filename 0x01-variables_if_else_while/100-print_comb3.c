ce68429dae3e148c42f0e561eb921a11bc3ff64a
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-03 15:31:05 -0700

    --

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Success
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 100; i++)
+	{
+		j = (i / 10) + '0';
+		k = (i % 10) +'0';
+		if ((!(j == k)) && j < k)
+		{
+			putchar(j);
+			putchar(k);
+			if (j != (8 + '0') && k != (10 + '0'))
+			{
+				putchar(',');
+			}
+			putchar(' ');
+		}
+	}
+	return (0);
+}
