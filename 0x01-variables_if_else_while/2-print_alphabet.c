cb8979028887cfeec6fa4854da0d2862a05ec0d3
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-03 11:51:54 -0700

    --

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - code to print lowercase letters
+ *
+ * Return: Success
+ */
+
+int main(void)
+{
+	char i = 'a';
+
+	while (i < 'z')
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
