3b2dfd3b936d264b9c5a5d616c0c045b4979739d
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-07 15:14:29 -0700

    --

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - function that prints last digit of a number
+ * @num: argument
+ * Return: Last digit of num
+ */
+
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
