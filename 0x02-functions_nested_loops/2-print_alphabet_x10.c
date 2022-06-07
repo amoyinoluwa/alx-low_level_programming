1116319242432b3c9ee782c2150fd4306ee58d16
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-07 09:54:40 -0700

    --

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print all alphabets ten times
+ *
+ * Return: Success
+ */
+
+void print_alphabet_x10(void)
+{
+	while (i < 10)
+	{
+		print_alphabet();
+	}
+	return (0);
+}
