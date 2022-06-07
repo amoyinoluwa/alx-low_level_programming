d7a6b218d52258dd8d7e57abfb43e6e948a13389
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-07 10:34:28 -0700

    --

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha - function to check if a character is an alphabet
+ *
+ * Return: 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	return (c >= 'A' && c <= 'z' ? 1 : 0);
+}
