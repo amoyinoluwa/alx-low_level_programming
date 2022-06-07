fc2415af61f7a4472bf9a3a4ea286e598f1afbb9
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-07 10:03:36 -0700

    --

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,16 @@
 void print_alphabet_x10(void)
 {
 	int i = 0;
+	char j = 'a';
 
 	while (i < 10)
 	{
-		print_alphabet();
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
 		i++;
 	}
 }
