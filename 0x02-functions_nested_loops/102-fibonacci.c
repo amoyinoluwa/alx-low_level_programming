e827356e1fd45af644f47dbeaaefc3d2d2160231
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-08 12:42:32 -0700

    --

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - main program
+ *
+ * Return: Success
+ */
+
+int main(void)
+{
+	int i;
+
+	unsigned long, second_last, last, temp;
+	second_last = 1;
+	last = 1;
+	printf("%d, ", 1);
+	for (i = 0; i < 49; i++)
+	{
+		temp = last;
+		last += second_last;
+		second_last = temp;
+		printf("%ld", last);
+		if (i != 48)
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
