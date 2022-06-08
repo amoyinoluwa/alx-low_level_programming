002e6aa75b34f50ff75ca360fd4f833a57eb5786
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-07 19:18:02 -0700

    --

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_to_98 - function to print numbers up to 98
+ * @n: number argument
+ * Return: numbers
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		printf("%d", 98);
+	}
+	else
+	{
+		while (98 < i)
+		{
+			printf("%d, ", i);
+			i--;
+		}
+		printf("%d", 98);
+	}
+}
