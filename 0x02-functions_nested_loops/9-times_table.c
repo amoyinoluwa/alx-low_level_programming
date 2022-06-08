5df54c613ecf01c765ee5c9114be151a38334954
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-07 17:42:40 -0700

    --

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - function to print the time table
+ *
+ * Return: none
+ */
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = i * j;
+			if (k > 10)
+			{
+				putchar((k / 10) + '0');
+				putchar((k % 10) + '0');
+			}
+			else
+			{
+				putchar((k % 10) + '0');
+			}
+			if (j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+}
+
