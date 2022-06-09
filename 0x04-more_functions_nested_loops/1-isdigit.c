f1c831bccfe62a9128f0dc712c04a8b02e4cd2fc
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-09 10:03:51 -0700

    --

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isdigit - function to check if a number is a digit
+ * @c: number argument
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	return ((c >= 0 && c <= 9) ? 1 : 0);
+}
