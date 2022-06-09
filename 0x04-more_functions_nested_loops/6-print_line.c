ae2d316d4206891be1f0e8965af97b8a8349f377
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-09 11:21:06 -0700

    --

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,13 @@ void print_line(int n)
 {
 	int i = 0;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		putchar('\n');
-		return;
-	}
-	while (i <= n)
-	{
-		putchar('-');
-		i++;
+		while (i < n)
+		{
+			putchar('-');
+			i++;
+		}
 	}
 	putchar('\n');
 }
