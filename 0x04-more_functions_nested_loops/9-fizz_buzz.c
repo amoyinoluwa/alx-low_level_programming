4e4b5594d6ebc126a02a092cc4360f5c0ab89351
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-09 13:43:04 -0700

    --

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - main prog
- *
+ * main - main program to print fizz buzz
+ * Description: mul to 3 is fizz, mul of 5 is buzz, 15 is fizzbuzz
  * Return: Success
  */
 
@@ -9,21 +9,21 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	i = 1;
+	while (i <= 100)
 	{
-		if ((i % 5) == 0 && (i % 3) == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		else if ((i % 3) == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else
-			printf("%d", i);
-		
+			print("%d", i);
 		if (i != 100)
 			printf(" ");
+		i++;
 	}
 	printf("\n");
-
 	return (0);
-}	
+}
