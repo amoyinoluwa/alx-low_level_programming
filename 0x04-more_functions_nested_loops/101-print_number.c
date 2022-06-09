e9203e55a2fffc2f085e33210a5e75982b18c701
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-09 14:04:05 -0700

    --

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,31 @@
 
 void print_number(int n)
 {
-	if (n < 0)
+	int digit, count, temp;
+	
+	digit = n;
+	count = 0;
+
+	if (digit < 0)
 	{
-		n *= -1;
 		_putchar('-');
 	}
-	if (n != 0)
+	for (temp = 1; digit > 9 || digit < -9; temp *= 10)
 	{
-		print_number(n / 10);
-		_putchar((n % 10) + '0');
+		digit /= 10;
+		count++;
+	}
+	for (digit = n; count >= 0; count--)
+	{
+		if (digit / temp < 0)
+		{
+			_putchar(((digit / temp) * -1) + '0');
+		}
+		else
+		{
+			_putchar(digit / temp + '0');
+		}
+		digit %= temp
+		temp /= 10;
 	}
 }
