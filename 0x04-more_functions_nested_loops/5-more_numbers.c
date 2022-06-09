096f805a5de4161ddf5aa02704ad5de2258f1f24
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-09 10:49:38 -0700

    --

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers
+ *
+ * Return: none
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if ( j >= 10)
+			{
+				putchar((j/10) + '0');
+				putchar((j%10) + '0');
+			}
+			else
+			{
+				putchar(j + '0');
+			}
+		}
+		putchar('\n');
+	}
+}
