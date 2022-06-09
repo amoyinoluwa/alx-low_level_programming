75de7a26387a9a6403078768acd6889ef2d30191
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-09 12:45:14 -0700

    --

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ *
+ * Return: none
+ */
+
+void print_triangle(int size)
+{
+	int row, col;
+
+	if (size > 0)
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (col = size - row; col > 0; col--)
+			{
+				putchar(' ');
+			}
+			for (col = 0; col < row; col++)
+			{
+				putchar('#');
+			}
+			if (row == size)
+			{
+				continue;
+			}
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+}
