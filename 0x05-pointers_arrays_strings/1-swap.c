2ad12c10ec1476617e5fec548dbfc9537ba7e653
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-13 08:37:13 -0700

    --

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
  * swap_int - function to swap
- * @a: first address
- * @b: second address
- * Return: Void
+ * @a: first int address
+ * @b: second int address
+ * Return: void
  */
 
 void swap_int(int *a, int *b)
 {
-	int *temp;
-	temp = a;
-	a = b;
-	b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
