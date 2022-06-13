7aa0227e98decefb740373ab38b5faa45a44eb81
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-13 11:46:01 -0700

    --

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_array - function to print elements of array
+ * @a: array
+ * @n: number of elements in array
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	putchar('\n');
+}
