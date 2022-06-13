32a98f7a42cabed2425cee8754e0f29321fe2f11
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-13 11:57:33 -0700

    --

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,31 @@
+/**
+ * _strcpy - string copy function
+ * @src - origin string
+ * @dest - destination string
+ * Return: copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int n = 0;
+	int i;
+
+	while (src[n] != '\0')
+	{
+		n++;
+	}
+	i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	i = i;
+	while (i <= n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
