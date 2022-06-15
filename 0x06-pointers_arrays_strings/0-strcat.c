eef7f79c15f2eb4efeda2232a4dae03a4f6ef9bd
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-15 10:13:14 -0700

    --

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - concatenates src str to dest str
+ * @dest: destination string
+ * @src: source string
+ * Return: Pointer to merged string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for(i = 0; dest[i] != '\0'; i++);
+	for(j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
