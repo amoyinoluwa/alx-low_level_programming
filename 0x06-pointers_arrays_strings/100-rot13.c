36407b406a321a6589c6656e74c3bd28f18b75e5
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-16 08:53:39 -0700

    --

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string with rot13
+ * @s: string args
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot_it[] =   "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; j < 52; j++)
+			if (s[i] == alphabet[j])
+				s[i] = rot_it[j], j = 52;
+	return (s);
+}
