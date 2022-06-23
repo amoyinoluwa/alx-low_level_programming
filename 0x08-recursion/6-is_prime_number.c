b8ac714630ddada81931ad6c98f612cbefcba81b
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-22 19:15:04 -0700

    --

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,33 @@
 #include "main.h"
 /**
+ * prime - prints prime
+ * @a: first int arg
+ * @b: second int arg
+ * Return: prime nums
+ */
+
+int prime(int a, int b)
+{
+	if (b == 1 || b == 25)
+	{
+		return (0);
+	}
+	else if (b <= 0)
+	{
+		return (0);
+	}
+	else if (b % 2 == 0)
+	{
+		return (0);
+	}
+	else if (b % 2 != 0)
+	{
+		return (1);
+	}
+	return (prime(a * (a + 1), b));
+}
+
+/**
  * is_prime_number - checks if input is prime
  * @n: int args
  * Return: 0 or 1
