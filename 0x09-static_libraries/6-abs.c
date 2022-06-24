9d42332a0ac17badd7f0fb2bb1eb22d6dffec351
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-24 15:39:03 -0700

    --

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - function to calculate absolute value of a number
+ * @i: number argument
+ * Return: Abs value
+ */
+
+int _abs(int i)
+{
+	return (i <= 0 ? i * -1 : i);
+}
