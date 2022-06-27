9a5e2c3604d2dd027091c23220c89fbfc980d7fb
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-27 16:29:05 -0700

    --

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strdup - return ptr to new str with copy of input
+ * @str: input string
+ * Return: copy of str
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+	char *copy = malloc((n + 1) * sizeof(char));
+
+	if (copy != NULL && str != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			copy[i] = str[i];
+		}
+		copy[i] = '\0';
+		return (copy);
+	}
+	return (NULL);
+}
