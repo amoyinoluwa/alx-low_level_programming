2cc780c92c38fb338285460527baf36ea028d5b0
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-27 17:56:15 -0700

    --

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,13 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k, n;
 	char *str = NULL;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	i = 0;
 	while (s1[i] != '\0')
