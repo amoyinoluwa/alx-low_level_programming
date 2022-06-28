9905bb766d98a9bb4e62da56b67d209ae000e4c7
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-28 09:31:54 -0700

    --

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 			arr[i] = malloc(width * sizeof(int));
 			if (arr[i] == NULL)
 			{
-				for (; i > 0; i--)
+				for (i--; i > 0; i--)
 					free(arr[i]);
 				free(arr);
 				return (NULL);
@@ -42,5 +42,5 @@ int **alloc_grid(int width, int height)
 			arr[i][j] = 0;
 		}
 	}
-	return (arr == NULL ? NULL : arr);
+	return (arr);
 }
