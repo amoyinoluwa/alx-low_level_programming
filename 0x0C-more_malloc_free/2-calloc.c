bf6ad1e9f05edf0b5c6d7f06062843ef353d63ca
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-30 11:33:22 -0700

    --

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,24 @@
 #include "main.h"
 /**
+ * _memset - set memory to byte
+ * @s: string to be filled
+ * @b: byte argument to fill in string
+ * @n: number of time to fill in byte
+ * Return: string with filled byte
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
  * _calloc - allocates memory for array
  * @nmemb: num of members in array
  * @size: size of data type
@@ -8,19 +27,13 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void **ptr = malloc(nmemb * size);
-	int i;
-	void *j;
+	void *ptr = malloc(nmemb * size);
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	if (ptr != NULL)
 	{
-		for (i = 0; i < nmemb * size; i++)
-		{
-			j = (ptr + i);
-			j = 0;
-		}
+		_memset(ptr, 0, nmemb)
 		return (ptr);
 	}
 	return (NULL);
