ee4a2e77e2cc5f3f7a55fb476ed39187eab48011
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-06-30 11:48:08 -0700

    --

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -31,12 +31,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
 	ptr = malloc(nmemb * size);
-
+	
 	if (ptr == NULL)
 		return (NULL);
-	
 	_memset(ptr, 0, (nmemb * size));
 	
 	return (ptr ? ptr : NULL);
