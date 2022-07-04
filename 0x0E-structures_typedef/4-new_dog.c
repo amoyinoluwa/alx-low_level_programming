e0207be8ae9b4dfb6a77ffea051fc56a1c40d90f
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-07-04 13:10:25 -0700

    --

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,59 @@
+#include "dog.h"
+/**
+ * _strdup - return ptr to new str with copy of input
+ * @str: input string
+ * Return: copy of str
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int n = 0;
+	char *copy = NULL;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+	copy = malloc((n + 1) * sizeof(char));
+	if (copy != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			copy[i] = str[i];
+		}
+		copy[i] = '\0';
+		return (copy);
+	}
+	return (NULL);
+}
+
+/**
+ * new_dog - creates a new dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	char *d_name, *d_owner;
+
+	d = malloc(sizeof(dog_t));
+	
+	if (d == NULL)
+		return (NULL);
+	
+	d_name = _strdup(name);
+	d_owner = _strdup(owner);
+
+	d->name = d_name;
+	d->age = age;
+	d->owner = d_owner;
+
+	return (d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,14 +13,15 @@
  * Description: Struct of dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
