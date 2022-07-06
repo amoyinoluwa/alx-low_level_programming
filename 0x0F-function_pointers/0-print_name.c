4dc00a1e71450a4c0e1dc051c1c16fa18dbae571
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-07-06 12:07:30 -0700

    --

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function to execute functions
+ * @f: function pointer
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
