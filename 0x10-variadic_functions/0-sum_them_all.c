0d85a8685c891528aa002f69ec7d73fab6f8a3cf
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-07-07 15:05:33 -0700

    --

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -19,7 +19,8 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(add, const unsigned int);
+		int val = va_arg(add, int);
+		sum += val;
 	}
 	
 	va_end(add);
